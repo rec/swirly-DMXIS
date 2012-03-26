0d6c0c4373f24ee229428c5d87224b3d3f0eee78
Tom Swirly <tom@swirly.com>
2012-03-26 14:48:56 -0400

    Start to abstract the xml tree

--- Python/MakePreset.py
+++ Python/MakePreset.py
@@ -3,29 +3,41 @@ import xml.etree.ElementTree as ElementTree
 
 import defaults
 
+USE_ETREE = True
+
+if USE_ETREE:
+  def CreateRootElement(tag, **attributes):
+    return ElementTree.Element(tag, attributes)
+
+  def CreateElement(parent, tag, **attributes):
+    return ElementTree.SubElement(parent, tag, attributes)
+
+  def Print(root, output):
+    ElementTree.ElementTree(root).write(output)
+
+
 def Add(names, data, tag, parent, nameField):
   sub = data[tag]
   for name in names:
     d = dict(sub.get(name, {}))
     d[nameField] = name
     d = dict((str(k), v) for k, v in d.iteritems())
-    df = defaults.Get(tag, **d)
-    ElementTree.SubElement(parent, tag, df)
+    CreateElement(parent, tag, **defaults.Get(tag, **d))
 
 
-def Translate(input, output):
+def MakePreset(input, output):
   data = json.load(input)
   d = defaults.Get('DbAudiowarePreset', name=data['name'])
-  root = ElementTree.Element('DbAudiowarePreset', d)
-  params = ElementTree.SubElement(root, 'Params')
-  dmx = ElementTree.SubElement(root, 'DmxUniverse')
+  root = CreateRootElement('DbAudiowarePreset', **d)
+  params = CreateElement(root, 'Params')
+  dmx = CreateElement(root, 'DmxUniverse')
 
   Add(defaults.PARAM_NAMES, data, 'Param', params, 'nm')
   Add(defaults.DMX_NAMES, data, 'c', dmx, 'n')
 
-  ElementTree.ElementTree(root).write(output)
+  Print(root, output)
 
 
 with open('sample.json') as input:
   with open('sample.xml', 'w') as output:
-    Translate(input, output)
+    MakePreset(input, output)
