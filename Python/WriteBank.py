b9e6743928ba117a10c20c8f8414a9a044cd0819
Tom Swirly <tom@swirly.com>
2012-03-27 16:34:47 -0400

    Start to move toward writing a whole bank file, and clean up some names

--- Python/WriteBank.py
+++ Python/WriteBank.py
@@ -34,10 +34,10 @@ def CreateElement(document, parent, tag, attributes={}):
   parent.appendChild(element)
   return element
 
-def Print(document, output):
+def WriteXml(document, output):
   output.write(document.toprettyxml(indent='  '))
 
-def Add(document, names, data, tag, parent, nameField):
+def AddElements(document, names, data, tag, parent, nameField):
   sub = data.get(tag, {})
   omit = set(sub.get("OMIT", []))
   for name in names:
@@ -47,27 +47,41 @@ def Add(document, names, data, tag, parent, nameField):
       attributes = defaults.GetDefault(tag, {nameField: name}, d)
       CreateElement(document, parent, tag, attributes)
 
-def MakePreset(name, data, output):
+def WritePreset(name, data, output):
   d = defaults.GetDefault('DbAudiowarePreset', dict(name=name), {})
   document = CreateDocument('DbAudiowarePreset', d)
   root = document.documentElement
   params = CreateElement(document, root, 'Params')
   dmx = CreateElement(document, root, 'DmxUniverse')
 
-  Add(document, defaults.PARAM_NAMES, data, 'Param', params, 'nm')
-  Add(document, defaults.DMX_NAMES, data, 'c', dmx, 'n')
+  AddElements(document, defaults.PARAM_NAMES, data, 'Param', params, 'nm')
+  AddElements(document, defaults.DMX_NAMES, data, 'c', dmx, 'n')
 
-  Print(root, output)
+  WriteXml(root, output)
 
-def WriteBank(bank):
+def WritePresetFile(name, data):
+  fname = name + '.prt'
+  with open(fname, 'w') as output:
+    WritePreset(name, data, output)
+  return fname
+
+def WritePresets(bank):
+  presets = []
   for name, subdata in bank.iteritems():
-    with open(name + '.xml', 'w') as output:
+    presets.append(fname)
+    with open(fname, 'w') as output:
       MakePreset(name, subdata, output)
+
+  return presets
+
+def WriteBank(bankName):
+  with open(bankName) as input:
+    bank = json.load(input)
+    presets = [WritePresetFile(name, data) for name, data in bank.iteritems()]
 
 if __name__ == '__main__':
   if len(sys.argv) is 2:
-    with open(sys.argv[1]) as input:
-      WriteBank(json.load(input))
+    WriteBank(sys.argv[1])
   else:
     print 'Usage: %s json-filename' % sys.argv[0]
     sys.exit(-1)
