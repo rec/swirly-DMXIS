989ebea7e3452291b95cd4e7f840c5870c109a91
Tom Swirly <tom@swirly.com>
2012-03-27 21:58:50 -0400

    Add Xml.py

---
+++ Python/Xml.py
@@ -0,0 +1,24 @@
+#!/usr/local/bin/python
+
+import xml.dom.minidom
+
+def SetNodeValue(node, kwds):
+  for k, v in kwds.iteritems():
+    # STUPID xml.dom.minidom sorts the attributes when it gets them.  :-(
+    node.setAttribute(k, v)
+
+def CreateDocument(tag, attributes={}):
+  impl = xml.dom.minidom.getDOMImplementation()
+  document = impl.createDocument(None, tag, None)
+  SetNodeValue(document.documentElement, attributes)
+  return document
+
+def CreateElement(document, parent, tag, attributes={}):
+  element = document.createElement(tag)
+  SetNodeValue(element, attributes)
+  parent.appendChild(element)
+  return element
+
+def WriteXml(document, output):
+  output.write(document.toprettyxml(indent='  '))
+
